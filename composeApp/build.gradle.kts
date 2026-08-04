80a2b89f26a4c7e8524d0e24e17186db60935b09
D. Ransom <djransom1@gmail.com>
2026-08-04 15:16:12 -0400

    Add Wasm web target and GitHub Pages deployment workflow

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,6 +20,17 @@ kotlin {
     
     jvm("desktop")
     
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+    wasmJs {
+        outputModuleName.set("composeApp")
+        browser {
+            commonWebpackConfig {
+                outputFileName = "composeApp.js"
+            }
+        }
+        binaries.executable()
+    }
+    
     listOf(
         iosX64(),
         iosArm64(),
