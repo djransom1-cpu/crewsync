42f405084f8ba679f8e266aa48d19b1271706b68
D. Ransom <djransom1@gmail.com>
2026-08-05 08:17:26 -0400

    Fix HTML canvas width and height buffer initialization in main.kt

--- composeApp/src/jsMain/kotlin/com/example/crewsync/main.kt
+++ composeApp/src/jsMain/kotlin/com/example/crewsync/main.kt
@@ -3,12 +3,25 @@ package com.example.crewsync
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import com.example.crewsync.util.initializeFirebase
+import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.HTMLCanvasElement
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     try {
         initializeFirebase()
     } catch (_: Throwable) {
+    }
+
+    val canvas = document.getElementById("ComposeTarget") as? HTMLCanvasElement
+    if (canvas != null) {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        window.addEventListener("resize", {
+            canvas.width = window.innerWidth
+            canvas.height = window.innerHeight
+        })
     }
 
     CanvasBasedWindow(title = "Crewsync Web", canvasElementId = "ComposeTarget") {
