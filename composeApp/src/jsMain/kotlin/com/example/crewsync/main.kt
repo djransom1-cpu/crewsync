eb7a4975e2ca96b694bba4284d272bb59ecc8230
D. Ransom <djransom1@gmail.com>
2026-08-06 08:24:41 -0400

    Switch to standard ComposeViewport(body) for universal browser DOM attachment

--- composeApp/src/jsMain/kotlin/com/example/crewsync/main.kt
+++ composeApp/src/jsMain/kotlin/com/example/crewsync/main.kt
@@ -1,8 +1,9 @@
 package com.example.crewsync
 
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
 import com.example.crewsync.util.initializeFirebase
+import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
@@ -11,7 +12,8 @@ fun main() {
     } catch (_: Throwable) {
     }
 
-    CanvasBasedWindow(title = "Crewsync", canvasElementId = "ComposeTarget") {
+    val body = document.body ?: return
+    ComposeViewport(body) {
         App()
     }
 }
