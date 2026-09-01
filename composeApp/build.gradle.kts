2dadf85ea8c0882e3c79536f3c24b6be8a13d61e
D. Ransom <djransom1@gmail.com>
2026-09-01 15:09:42 -0400

    Bump desktop packageVersion to 1.0.4

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -166,7 +166,7 @@ compose.desktop {
             includeAllModules = true
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "Crewsync" // Changed from com.djransom.crewsync
-            packageVersion = "1.0.3"
+            packageVersion = "1.0.4"
             description = "Construction Crew Management"
             copyright = "© 2026 Crewsync Team"
             vendor = "Crewsync"
